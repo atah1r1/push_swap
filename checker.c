c8b01ecbb847a750346244934debf5ac8eb86c8e
mac <atahiri@student.1337.ma>
2021-03-13 19:19:23 +0100

    implementing stack

--- checker.c
+++ checker.c
@@ -6,19 +6,114 @@
 /*   By: mac <mac@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/11 14:12:02 by atahiri           #+#    #+#             */
-/*   Updated: 2021/03/12 11:55:18 by mac              ###   ########.fr       */
+/*   Updated: 2021/03/13 17:07:48 by mac              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int     main(int argc, char **argv)
+// Data structure to represent a stack
+struct stack
 {
-	int i = 1;
-	while (i < argc)
+	int maxsize;	// define max capacity of the stack
+	int top;
+	int *items;
+};
+
+// Utility function to initialize the stack
+struct stack* newStack(int capacity)
+{
+	struct stack *pt = (struct stack*)malloc(sizeof(struct stack));
+
+	pt->maxsize = capacity;
+	pt->top = -1;
+	pt->items = (int*)malloc(sizeof(int) * capacity);
+
+	return pt;
+}
+
+// Utility function to return the size of the stack
+int size(struct stack *pt) {
+	return pt->top + 1;
+}
+
+// Utility function to check if the stack is empty or not
+int isEmpty(struct stack *pt) {
+	return pt->top == -1;   				// or return size(pt) == 0;
+}
+
+// Utility function to check if the stack is full or not
+int isFull(struct stack *pt) {
+	return pt->top == pt->maxsize - 1;  	// or return size(pt) == pt->maxsize;
+}
+
+// Utility function to add an element `x` to the stack
+void push(struct stack *pt, int x)
+{
+	// check if the stack is already full. Then inserting an element would
+	// lead to stack overflow
+	if (isFull(pt))
 	{
-    	printf("%s\n", argv[i]);
-		i++;
+		printf("Overflow\nProgram Terminated\n");
+		exit(EXIT_FAILURE);
 	}
-    return 0;
+
+	printf("Inserting %d\n", x);
+
+	// add an element and increment the top's index
+	pt->items[++pt->top] = x;
+}
+
+// Utility function to return the top element of the stack
+int peek(struct stack *pt)
+{
+	// check for an empty stack
+	if (!isEmpty(pt)) {
+		return pt->items[pt->top];
+	}
+	else {
+		exit(EXIT_FAILURE);
+	}
+}
+
+// Utility function to pop a top element from the stack
+int pop(struct stack *pt)
+{
+	// check for stack underflow
+	if (isEmpty(pt))
+	{
+		printf("Underflow\nProgram Terminated\n");
+		exit(EXIT_FAILURE);
+	}
+
+	printf("Removing %d\n", peek(pt));
+
+	// decrement stack size by 1 and (optionally) return the popped element
+	return pt->items[pt->top--];
+}
+
+int main()
+{
+	// create a stack of capacity 5
+	struct stack *pt = newStack(5);
+
+	push(pt, 1);
+	push(pt, 2);
+	push(pt, 3);
+
+	printf("The top element is %d\n", peek(pt));
+	printf("The stack size is %d\n", size(pt));
+
+	pop(pt);
+	pop(pt);
+	pop(pt);
+
+	if (isEmpty(pt)) {
+		printf("The stack is empty");
+	}
+	else {
+		printf("The stack is not empty");
+	}
+
+	return 0;
 }
--- push_swap.h
+++ push_swap.h
@@ -3,19 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: atahiri <atahiri@student.42.fr>            +#+  +:+       +#+        */
+/*   By: mac <mac@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/11 12:34:41 by atahiri           #+#    #+#             */
-/*   Updated: 2021/03/11 14:11:51 by atahiri          ###   ########.fr       */
+/*   Updated: 2021/03/13 17:04:38 by mac              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+typedef	struct	s_stack {
+    int		maxsize;
+    int		top;
+    int		*items;
+}				t_stack;
 
 #endif
