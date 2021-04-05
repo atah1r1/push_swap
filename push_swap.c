a69a1a3957413dddb3cfc13b58795fe5d4b86aac
Amine Tahiri <atahiri@student.1337.ma>
2021-04-05 16:00:04 +0100

    start push_swap

--- push_swap.c
+++ push_swap.c
@@ -6,13 +6,13 @@
 /*   By: atahiri <atahiri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/14 10:50:53 by atahiri           #+#    #+#             */
-/*   Updated: 2021/03/16 11:31:01 by atahiri          ###   ########.fr       */
+/*   Updated: 2021/04/05 15:58:35 by atahiri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int     main()
+int     main(int argc, char **argv)
 {
     printf("");
     return 0;
